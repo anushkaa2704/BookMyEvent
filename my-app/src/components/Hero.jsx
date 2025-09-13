c9602137f1b084d83e47c94d324bb41b140c09c5
Anushka Patil <anushkaa2704@gmail.com>
2025-09-13 18:44:54 +0530

    changed hero.jsx and css

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import './Hero.css'
+import React from 'react';
+import './Hero.css';
+import heroBg from "../images/hero-bg.jpeg";
 
 const Hero = () => {
   return (
-    <section className="hero">
+    <section
+      className="hero"
+      style={{
+        backgroundImage: `linear-gradient(rgba(26, 18, 11, 0.7), rgba(26, 18, 11, 0.7)), url(${heroBg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+    >
       <div className="container">
         <h1>Crafting Unforgettable Moments</h1>
         <p>BookMyEvent transforms your vision into an unforgettable celebration of love</p>
@@ -25,7 +33,7 @@ const Hero = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
