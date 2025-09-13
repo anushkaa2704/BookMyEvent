5aaf6e9c3ca8505e19dd1c604aff19ffaa91c2f9
Anushka Patil <anushkaa2704@gmail.com>
2025-09-13 18:58:14 +0530

    vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 👈 important for Vercel/GitHub Pages deployment
+  base: "./",  // 👈 ensures assets load correctly when deployed
 })
